e82f553c035c5df7beee6d69ab5fe4520e2c3494
Sandra Ptaszek <staszek@desmart.com>
2016-11-11 21:16:00 +0100

    created arrow hamburger-menu

--- examples/hamburger-menu/app.js
+++ examples/hamburger-menu/app.js
@@ -1,20 +1,38 @@
 $(document).ready(function() {
 
-  $('#menuButton').click(function () {
-    if ($('#line1').hasClass('button--open-line1') && $('#line3').hasClass('button--open-line3')) {
-      $('#line1').removeClass('button--open-line1');
-      $('#line1').addClass('button--close-line1');
-      $('#line2').removeClass('button--open-line2');
-      $('#line2').addClass('button--close-line2');
-      $('#line3').removeClass('button--open-line3');
-      $('#line3').addClass('button--close-line3');
+  $('#fadeButton').click(function () {
+    if ($('#fadeLine1').hasClass('button--fade--open-line1') && $('#fadeLine3').hasClass('button--fade--open-line3')) {
+      $('#fadeLine1').removeClass('button--fade--open-line1');
+      $('#fadeLine1').addClass('button--fade--close-line1');
+      $('#fadeLine2').removeClass('button--fade--open-line2');
+      $('#fadeLine2').addClass('button--fade--close-line2');
+      $('#fadeLine3').removeClass('button--fade--open-line3');
+      $('#fadeLine3').addClass('button--fade--close-line3');
     } else {
-      $('#line1').addClass('button--open-line1');
-      $('#line1').removeClass('button--close-line1');
-      $('#line2').addClass('button--open-line2');
-      $('#line2').removeClass('button--close-line2');
-      $('#line3').addClass('button--open-line3');
-      $('#line3').removeClass('button--close-line3');
+      $('#fadeLine1').addClass('button--fade--open-line1');
+      $('#fadeLine1').removeClass('button--fade--close-line1');
+      $('#fadeLine2').addClass('button--fade--open-line2');
+      $('#fadeLine2').removeClass('button--fade--close-line2');
+      $('#fadeLine3').addClass('button--fade--open-line3');
+      $('#fadeLine3').removeClass('button--fade--close-line3');
+    }
+  })
+
+  $('#arrowButton').click(function () {
+    if ($('#arrowLine1').hasClass('button--arrow--open-line1') && $('#arrowLine3').hasClass('button--arrow--open-line3')) {
+      $('#arrowLine1').removeClass('button--arrow--open-line1');
+      $('#arrowLine1').addClass('button--arrow--close-line1');
+      $('#arrowLine2').removeClass('button--arrow--open-line2');
+      $('#arrowLine2').addClass('button--arrow--close-line2');
+      $('#arrowLine3').removeClass('button--arrow--open-line3');
+      $('#arrowLine3').addClass('button--arrow--close-line3');
+    } else {
+      $('#arrowLine1').addClass('button--arrow--open-line1');
+      $('#arrowLine1').removeClass('button--arrow--close-line1');
+      $('#arrowLine2').addClass('button--arrow--open-line2');
+      $('#arrowLine2').removeClass('button--arrow--close-line2');
+      $('#arrowLine3').addClass('button--arrow--open-line3');
+      $('#arrowLine3').removeClass('button--arrow--close-line3');
     }
   })
 
