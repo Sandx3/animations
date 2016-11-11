a300f7c96387a520b41efe28f49e93aecb8842ac
Sandra Ptaszek <staszek@desmart.com>
2016-11-11 19:21:04 +0100

    tested anchors and js options

--- examples/fullpagejs-library-test/app.js
+++ examples/fullpagejs-library-test/app.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-  $('#fullpage').fullpage();
+  $('#fullpage').fullpage({
+    anchors:['hello', 'world'],
+    controlArrows: false,
+    sectionsColor: ['#7ebb6e', '#f7a766']
+  });
 });
